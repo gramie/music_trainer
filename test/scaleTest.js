5b155e05ea890a0a246d31671ffa47000405a760
gramie <gramie2@email.com>
2022-09-08 19:12:01 -0400

    Add pentatonic and blues scales

--- test/scaleTest.js
+++ test/scaleTest.js
@@ -14,6 +14,11 @@ QUnit.module('key', function() {
         assert.deepEqual(scale.notes, ['D', 'E', 'F', 'G', 'A', 'B♭', 'C']);
     });
 
+    QUnit.test('A minor test', function(assert) {
+        const scale = new Scale('A', 'minor');
+        assert.deepEqual(scale.notes, ['A', 'B', 'C', 'D', 'E', 'F', 'G']);
+    });
+
     QUnit.test('B♭ major test', function(assert) {
         const scale = new Scale('B♭', 'major');
         assert.deepEqual(scale.notes, ['B♭', 'C', 'D', 'E♭', 'F', 'G', 'A']);
@@ -27,6 +32,22 @@ QUnit.module('key', function() {
 
     QUnit.test('B♭ pentatonic major test', function(assert) {
         const scale = new Scale('B♭', 'pentatonic-major');
-        assert.deepEqual(scale.notes, ['B♭', 'C', 'D', 'F', 'G♭']);
+        assert.deepEqual(scale.notes, ['B♭', 'C', 'D', 'F', 'G']);
     });
+
+    QUnit.test('F# pentatonic major test', function(assert) {
+        const scale = new Scale('F#', 'pentatonic-major');
+        assert.deepEqual(scale.notes, ['F#', 'G#', 'A#', 'C#', 'D#']);
+    });
+
+    QUnit.test('A pentatonic minor test', function(assert) {
+        const scale = new Scale('A', 'pentatonic-minor');
+        assert.deepEqual(scale.notes, ['A', 'C', 'D', 'E', 'G']);
+    });
+
+    QUnit.test('C Blues test', function(assert) {
+        const scale = new Scale('C', 'blues');
+        assert.deepEqual(scale.notes, ['C', 'E♭', 'F', 'G♭', 'G', 'B♭']);
+    });
+
 });
